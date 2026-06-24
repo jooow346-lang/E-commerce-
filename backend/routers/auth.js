37ec79444d1b9cebb744c63450d6a987e30e69ef
jowcode <jooow346@gamil.com>
2026-06-25 00:43:22 +0300

    make a logout auth router and test it

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -57,13 +57,77 @@ router.post('/login' , async (req , res) => {
     }
 
     const accessToken = await jwt.sign({_id : user._id} , process.env.ACCESS_TOKEN_SECRET,{expiresIn : '15m'});
-    const refreshToken = await jwt.sign({_id:user._id} , process.env.REFRESH_TOKEN_SECRET,{expiresIn : '7d'})    res.status(200).json({token , user});
+    const refreshToken = await jwt.sign({_id:user._id} , process.env.REFRESH_TOKEN_SECRET,{expiresIn : '7d'})  
+    user.refreshToken = refreshToken;
+    await user.save();  
+    res.status(200).json({accessToken , refreshToken});
 }
 catch(error){
     res.status(500).json({message : error.message});
 }
 
 
+})
+
+
+//refresh token
+
+
+router.post('/refresh' , async (req , res) => {
+    try{
+        
+        const refreshToken = req.body.refreshToken;
+        const decoded  = await jwt.verify(refreshToken , process.env.REFRESH_TOKEN_SECRET);
+       
+        const user = await User.findById(decoded._id);
+         const accessToken = await jwt.sign({_id : decoded ._id} , process.env.ACCESS_TOKEN_SECRET , {expiresIn : '15m'});
+         if (!refreshToken) {
+          return res.status(401).json({
+            message: "Refresh token required"
+          });
+        }
+        if (!user) {
+          return res.status(404).json({
+            message: "User not found"
+          });
+        }
+
+        if (user.refreshToken !== refreshToken) {
+          return res.status(403).json({
+            message: "Invalid refresh token"
+          });
+        }
+                res.status(200).json({accessToken});
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
+})
+
+//logout
+
+router.post('/logout', async (req, res) => {
+  try{
+
+    const refreshToken = req.body.refreshToken;
+    if(!refreshToken){
+      return res.status(400).json({message: "Refresh token required"});
+    }
+    
+    const user = await User.findOne({refreshToken});
+    if(!user){
+      return res.status(400).json({message: "User not found"});
+    }
+    user.refreshToken="";
+    await user.save();
+    res.status(200).json({message: "Logout successful"});
+
+    }
+
+  
+  catch(error){
+    res.status(500).json({message : error.message});
+  }
 })
 
 
